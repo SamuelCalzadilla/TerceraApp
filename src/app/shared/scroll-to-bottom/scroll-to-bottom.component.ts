38e7b49917162b4748265526f72dfe9db3966619
Samuel Calzadilla Orcero <scalzadillaorcero@gmail.com>
2024-12-21 19:32:42 +0100

    Desaparición botones subir y bajar

--- src/app/shared/scroll-to-bottom/scroll-to-bottom.component.ts
+++ src/app/shared/scroll-to-bottom/scroll-to-bottom.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-scroll-to-bottom',
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./scroll-to-bottom.component.css']
 })
 export class ScrollToBottomComponent {
+  isVisible: boolean = true; // Controla la visibilidad del botón
+
+  // Escucha el evento de scroll en la ventana
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollPosition = window.pageYOffset + window.innerHeight;
+    const pageHeight = document.body.scrollHeight;
+    const threshold = 50; // Umbral para considerar la cercanía al final
+
+    // Si estás cerca del final, oculta el botón
+    this.isVisible = pageHeight - scrollPosition > threshold;
+  }
+
   // Método para hacer scroll hacia abajo
   scrollToBottom(): void {
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
--- src/app/shared/scroll-to-top/scroll-to-top.component.ts
+++ src/app/shared/scroll-to-top/scroll-to-top.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-scroll-to-top',
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./scroll-to-top.component.css']
 })
 export class ScrollToTopComponent {
+  isVisible: boolean = false; // Controla la visibilidad del botón
+
+  // Escucha el evento de scroll en la ventana
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    // Muestra el botón si la posición del scroll es mayor a 20px
+    this.isVisible = window.pageYOffset > 20;
+  }
+
   // Método para hacer scroll hacia arriba
   scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
