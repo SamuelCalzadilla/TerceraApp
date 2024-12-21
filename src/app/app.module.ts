505937d9c5475c1214198529dd06683c95c85337
Samuel Calzadilla Orcero <scalzadillaorcero@gmail.com>
2024-12-21 19:57:03 +0100

    Carga e páginas al inicio arriba del todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule, Router, NavigationEnd } from '@angular/router'; // Asegúrate de importar Router y NavigationEnd
+
 import { SharedModule } from './shared/shared.module';
 import { PaginaEjemplos1Component } from './ejemplos/pagina-ejemplos-1/pagina-ejemplos-1.component';
 import { PaginaEjemplos2Component } from './ejemplos/pagina-ejemplos-2/pagina-ejemplos-2.component';
@@ -19,11 +21,21 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    RouterModule 
   ],
   providers: [
     provideClientHydration(withEventReplay())
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(router: Router) {
+    // Habilitar scrollRestoration
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0); // Asegurarse de que el scroll esté en la parte superior al finalizar la navegación
+      }
+    });
+  }
+}
