90910f1fe938dbdf88ed51b0799d81aa9b6c8e60
Samuel Calzadilla Orcero <scalzadillaorcero@gmail.com>
2024-12-21 20:07:58 +0100

    Solucion carga de paginas parte navegador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 
 import { RouterModule, Router, NavigationEnd } from '@angular/router'; // Asegúrate de importar Router y NavigationEnd
 
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, Inject } from '@angular/core';
+
 import { SharedModule } from './shared/shared.module';
 import { PaginaEjemplos1Component } from './ejemplos/pagina-ejemplos-1/pagina-ejemplos-1.component';
 import { PaginaEjemplos2Component } from './ejemplos/pagina-ejemplos-2/pagina-ejemplos-2.component';
@@ -30,10 +33,10 @@ import { HomeComponent } from './home/home.component';
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(router: Router) {
-    // Habilitar scrollRestoration
+  constructor(router: Router, @Inject(PLATFORM_ID) private platformId: Object) {
+    // Habilitar scrollRestoration solo en el navegador
     router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
+      if (event instanceof NavigationEnd && isPlatformBrowser(this.platformId)) {
         window.scrollTo(0, 0); // Asegurarse de que el scroll esté en la parte superior al finalizar la navegación
       }
     });
